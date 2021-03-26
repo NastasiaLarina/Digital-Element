cc035f47f94607a64b31906b079ae792525d2fbd
NastasiaLarina <stucha84@mail.ru>
2021-03-26 12:40:17 +0500

    New task, step 1, 2

---
+++ Digital Element/Controller/ItemViewController.swift
@@ -0,0 +1,59 @@
+//
+//  IthemViewController.swift
+//  Digital Element
+//
+//  Created by Анастасия Ларина on 25.03.2021.
+//
+
+import UIKit
+
+class ItemViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical //.horizontal
+            layout.minimumLineSpacing = 5
+            layout.minimumInteritemSpacing = 5
+            collectionView.setCollectionViewLayout(layout, animated: true)
+    }
+    
+    // MARK: - Collection View
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ItemCell
+        
+
+        cell.articleItemLabel.text = "459827\(indexPath.row)"
+        cell.nameItemLabel.text = "Рубашка белая, из вискозы, 44"
+        cell.priceItemLamel.text = "75 руб."
+        cell.imageItemView.layer.cornerRadius = 10.0
+    
+        return cell
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)//here your custom value for spacing
+        }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+                let lay = collectionViewLayout as! UICollectionViewFlowLayout
+                let widthPerItem = collectionView.frame.width / 2 - lay.minimumInteritemSpacing
+
+    return CGSize(width:widthPerItem, height:367)
+   
+    }
+    
+}
+
+
+
---
+++ Digital Element/View/Cells/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  IthemCell.swift
+//  Digital Element
+//
+//  Created by Анастасия Ларина on 25.03.2021.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageItemView: UIImageView!
+    @IBOutlet weak var articleItemLabel: UILabel!
+    @IBOutlet weak var nameItemLabel: UILabel!
+    @IBOutlet weak var priceItemLamel: UILabel!
+
+
+}
+
+
